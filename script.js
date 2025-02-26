600874ac458992c2697ca0cb02d757128055e3b6
MinhThu0301 <lhmt1403@gmail.com>
2025-02-26 11:13:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function login ()
   {
     let a = usernam.value
-    let b= password. value
-    if ( a=="abc" && b == "123" )
+    let b= password.value
+    if ( a=="abc" && b=="123" )
     {
       window.open("home.html")
       document. get ElementById(compiler).style.display="block";
