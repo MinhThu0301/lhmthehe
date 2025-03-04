9fac463760e95a9f92a6a06b60e5c4e8b9d1d3c7
MinhThu0301 <lhmt1403@gmail.com>
2025-03-04 18:34:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function login ()
   {
-    let a = document.getElementById("username").value;
-    let b = document.getElementById("password").value;
-
+    let a = username.value
+    let b= password.value
     if ( a == "abc" && b == "1234" )
     {
-      window.location.href = "home.html";
-     document.getElementById("compiler").style.display = "block";
+      window.open("home.html")
+      document. getElementById(compiler).style.display="block";
     }
-   document.getElementById("error-message").innerText = "Không hợp lệ";
+    else document.write("Không hợp lệ") 
   } 
